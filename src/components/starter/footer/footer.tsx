05c9feac85b25c71c97861e56b33fe57e63cdfcf
Austin Rooks <Austionian@users.noreply.github.com>
2023-03-21 22:41:41 -0500

    fix deleted export

--- src/components/starter/footer/footer.tsx
+++ src/components/starter/footer/footer.tsx
@@ -1,5 +1,4 @@
 import { component$ } from "@builder.io/qwik";
-import { useServerTimeLoader } from "~/routes/layout";
 import styles from "./footer.module.css";
 
 export default component$(() => {
