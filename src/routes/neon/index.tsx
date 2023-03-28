a25e0fb5a90111961ce0cf07fa4bb1f5babe1268
Austin Rooks <Austionian@users.noreply.github.com>
2023-03-27 22:20:27 -0500

    use route loader

--- src/routes/neon/index.tsx
+++ src/routes/neon/index.tsx
@@ -1,43 +1,36 @@
-import { component$, Resource, useResource$ } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
+import { routeLoader$ } from "@builder.io/qwik-city";
 import getAPIKey from "~/helpers/getAPIKey";
 
 interface Fish {
   name: string;
 }
 
-export default component$(() => {
+export const useNeonData = routeLoader$<Fish[]>(async () => {
   const token = getAPIKey();
-  const fishResource = useResource$<Fish[]>(async ({ cleanup }) => {
-    const abortController = new AbortController();
-    cleanup(() => abortController.abort("cleanup"));
-    const res = await fetch(
-      "https://fishy-edge-tvp4i.ondigitalocean.app/v1/fishs",
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-        signal: abortController.signal,
-      }
-    );
-    return res.json();
-  });
+
+  const res = await fetch(
+    "https://fishy-edge-tvp4i.ondigitalocean.app/v1/fishs",
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  return res.json();
+});
+
+export default component$(() => {
+  const fishData = useNeonData();
 
   return (
     <div>
       <ul>
-        <Resource
-          value={fishResource}
-          onPending={() => <div>Loading...</div>}
-          onResolved={(data) => {
-            return (
-              <ul>
-                {data.map((fish, i) => (
-                  <li key={i}>{fish.name}</li>
-                ))}
-              </ul>
-            );
-          }}
-        />
+        <ul>
+          {fishData &&
+            fishData.value.map((fish, i) => <li key={i}>{fish.name}</li>)}
+        </ul>
       </ul>
     </div>
   );
