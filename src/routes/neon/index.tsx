ac379337722b2886f89a4882661b2b35c8ff946d
Austin Rooks <Austionian@users.noreply.github.com>
2023-03-29 15:28:15 -0500

    use route loader to give useResource api key

--- src/routes/neon/index.tsx
+++ src/routes/neon/index.tsx
@@ -1,36 +1,46 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, Resource, useResource$ } from "@builder.io/qwik";
 import { routeLoader$ } from "@builder.io/qwik-city";
 import getAPIKey from "~/helpers/getAPIKey";
+import type Fish from "~/types/Fish";
 
-interface Fish {
-  name: string;
-}
-
-export const useNeonData = routeLoader$<Fish[]>(async ({ env }) => {
-  const token = getAPIKey(env);
-
-  const res = await fetch(
-    "https://fishy-edge-tvp4i.ondigitalocean.app/v1/fishs",
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }
-  );
-
-  return res.json();
+export const useApiKey = routeLoader$(async ({ env }) => {
+  return getAPIKey(env);
 });
 
 export default component$(() => {
-  const fishData = useNeonData();
+  const apiKey = useApiKey();
+  const fishResource = useResource$<Fish[]>(async () => {
+    const res = await fetch(
+      "https://fishy-edge-tvp4i.ondigitalocean.app/v1/fishs",
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey.value}`,
+        },
+      }
+    );
+    return res.json();
+  });
 
   return (
     <div>
+      <h1 class="text-black">Hello</h1>
       <ul>
-        <ul>
-          {fishData &&
-            fishData.value.map((fish, i) => <li key={i}>{fish.name}</li>)}
-        </ul>
+        <Resource
+          value={fishResource}
+          onPending={() => <div>Loading...</div>}
+          onResolved={(data) => {
+            return (
+              <ul>
+                {data.map((fish, i) => (
+                  <li key={i}>{fish.name}</li>
+                ))}
+              </ul>
+            );
+          }}
+          onRejected={() => (
+            <div>Failed to load fish data. Please refresh your page.</div>
+          )}
+        />
       </ul>
     </div>
   );
