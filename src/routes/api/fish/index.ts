beec7176f2b4e23880e1d90478e3cf809c2d977c
Austin Rooks <Austionian@users.noreply.github.com>
2023-03-31 09:50:57 -0500

    try api

--- src/routes/api/fish/index.ts
+++ src/routes/api/fish/index.ts
@@ -1,15 +1,19 @@
 import type { RequestHandler } from "@builder.io/qwik-city";
-// import getAPIKey from "~/helpers/getAPIKey";
+import getAPIKey from "~/helpers/getAPIKey";
 import type Fish from "~/types/Fish";
 
-export const onGet: RequestHandler<Fish[]> = async ({ json }) => {
-  // const apiKey = "c0934beac2979a5740b175d96aeff4ed4b057860";
-  // const res = await fetch("https://mcwfishapp.com/fishs/", {
-  //   headers: {
-  //     Authorization: `Token ${apiKey}`,
-  //   },
-  // });
+export const onGet: RequestHandler<Fish[]> = async ({ json, env }) => {
+  const apiKey = getAPIKey(env);
+  const res = await fetch(
+    "https://fishy-edge-tvp4i.ondigitalocean.app/v1/fishs",
+    {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
+    }
+  );
+  const data: Fish[] = await res.json();
   json(200, {
-    test: "test",
+    data,
   });
 };
--- src/routes/neon/index.tsx
+++ src/routes/neon/index.tsx
@@ -1,30 +1,47 @@
-import { component$ } from "@builder.io/qwik";
-import { routeLoader$ } from "@builder.io/qwik-city";
-import getAPIKey from "~/helpers/getAPIKey";
+import { component$, Resource, useResource$ } from "@builder.io/qwik";
+// import { routeLoader$ } from "@builder.io/qwik-city";
+// import getAPIKey from "~/helpers/getAPIKey";
 import type Fish from "~/types/Fish";
 
-export const useFishApi = routeLoader$<Fish[]>(async ({ env }) => {
-  const apiKey = getAPIKey(env);
-  const res = await fetch(
-    "https://fishy-edge-tvp4i.ondigitalocean.app/v1/fishs",
-    {
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-      },
-    }
-  );
-  return res.json();
-});
+// export const useFishApi = routeLoader$<Fish[]>(async ({ env }) => {
+//   const apiKey = getAPIKey(env);
+//   const res = await fetch(
+//     "https://fishy-edge-tvp4i.ondigitalocean.app/v1/fishs",
+//     {
+//       headers: {
+//         Authorization: `Bearer ${apiKey}`,
+//       },
+//     }
+//   );
+//   return res.json();
+// });
+
+export const getFish = async () => {
+  const res = await fetch("/api/fish");
+  const data = await res.json();
+  return data;
+};
 
 export default component$(() => {
-  const fishData = useFishApi();
+  // const fishData = useFishApi();
+  const fishResource = useResource$<Fish[]>(async () => {
+    return await getFish();
+  });
   return (
     <div>
       <h1 class="text-black">Hello</h1>
       <ul>
-        {fishData.value.map((fish, i) => (
-          <li key={i}>{fish.name}</li>
-        ))}
+        <Resource
+          value={fishResource}
+          onPending={() => <div>Loading...</div>}
+          onResolved={(fishs) => (
+            <>
+              {fishs.map((fish, i) => (
+                <li key={i}>{fish.name}</li>
+              ))}
+            </>
+          )}
+        />
       </ul>
     </div>
   );
