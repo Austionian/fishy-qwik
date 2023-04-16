9367919602e36b72ea899eb732c1aed6e050981a
Austin Rooks <Austionian@users.noreply.github.com>
2023-04-16 16:32:07 -0500

    add fallback to cookie getter

--- src/helpers/getCookie.ts
+++ src/helpers/getCookie.ts
@@ -1,6 +1,8 @@
 export default function getCookie(cookie: string) {
-  return document.cookie
+  const value = document.cookie
     .split("; ")
     .find((row) => row.startsWith(`${cookie}=`))
     ?.split("=")[1];
+
+  return value ? value : "";
 }
