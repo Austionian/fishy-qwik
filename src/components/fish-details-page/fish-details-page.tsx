a99e862b9d47647b4c7fd2ebf4186621a10cc340
Austin Rooks <Austionian@users.noreply.github.com>
2023-04-28 16:04:31 -0500

    start on details page

--- src/components/fish-details-page/fish-details-page.tsx
+++ src/components/fish-details-page/fish-details-page.tsx
@@ -4,8 +4,6 @@ import type Fish from "~/types/Fish";
 import type UserDetails from "~/types/UserDetails";
 import type Recipe from "~/types/Recipe";
 
-import FishDetails from "~/components/fish-details/fish-details";
-
 type FishData = {
   fish_data: Fish;
   recipe_data: Recipe[];
@@ -18,25 +16,25 @@ type Props = {
 export default component$(({ fishData, userDetails }: Props) => (
   <div class="min-h-full">
     <main class="pb-10">
-      <div class="mx-auto max-w-3xl px-4 sm:px-6 md:flex md:items-center md:justify-between md:space-x-5 lg:max-w-7xl lg:px-8">
+      <div class="mx-auto max-w-3xl md:block md:items-center md:justify-between md:space-x-5 lg:max-w-7xl">
         <div class="flex justify-center items-center flex-wrap space-x-5">
-          <div class="flex-shrink-0">
-            <div class="relative">
-              {fishData.fish_data.woodland_fish_image ? (
-                <img
-                  class="h-32 sm:h-56"
-                  src={`/images/${fishData.fish_data.woodland_fish_image}`}
-                />
-              ) : (
-                <img
-                  class="h-32 sm:h-56"
-                  src={`/images/${fishData.fish_data.fish_image}`}
-                />
-              )}
-            </div>
+          <div class="rounded mb-4 w-screen flex justify-center">
+            {fishData.fish_data.woodland_fish_image ? (
+              <img
+                class="h-40 sm:w-[50%] sm:h-[100%] text-center"
+                src={`/images/${fishData.fish_data.woodland_fish_image}`}
+              />
+            ) : (
+              <img
+                class="h-40 sm:w-[50%] sm:h-[100%] text-center"
+                src={`/images/${fishData.fish_data.fish_image}`}
+              />
+            )}
           </div>
-          <div>
-            <h1 class="text-4xl font-bold">
+        </div>
+        <div>
+          <div class="flex justify-between items-center">
+            <h1 class="text-5xl font-extralight">
               {fishData.fish_data.anishinaabe_name ? (
                 <>
                   {fishData.fish_data.anishinaabe_name}
@@ -46,59 +44,152 @@ export default component$(({ fishData, userDetails }: Props) => (
                 fishData.fish_data.name
               )}
             </h1>
-          </div>
-        </div>
-        <div class="mt-10 sm:mt-6 flex flex-col-reverse justify-stretch space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-end sm:space-x-3 sm:space-y-0 sm:space-x-reverse md:mt-0 md:flex-row md:space-x-3">
-          <button
-            type="button"
-            class="inline-flex items-center justify-center rounded-md bg-teal-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="100pt"
-              height="100pt"
-              version="1.1"
-              viewBox="0 0 100 100"
-              class="h-6 w-6 mr-2"
-              fill="white"
+            <button
+              type="button"
+              class="inline-flex items-center justify-center rounded-md bg-teal-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600"
             >
-              <path d="m25.551 29.172c-5.9766 5.9766-5.9961 15.688-0.0625 21.691l0.003907 0.003907 20.738 22.766c0.99219 1.1016 2.3281 1.707 3.7656 1.707s2.7734-0.60547 3.7578-1.6992l20.75-22.773c5.9336-6.0039 5.9141-15.715-0.058594-21.695-5.9688-5.9648-15.668-5.9922-21.672-0.078125l-2.7734 2.3281-2.7734-2.3281c-6.0039-5.9141-15.703-5.8906-21.676 0.078125zm46.34 2.5547c4.5859 4.5859 4.5859 12.051 0 16.637-0.011719 0.011719-0.019531 0.023438-0.03125 0.03125l-0.027344 0.027344-20.758 22.781c-0.29688 0.32812-0.67969 0.51172-1.0742 0.51172-0.39844 0-0.78125-0.18359-1.0859-0.51953l-20.742-22.77c-0.011719-0.011719-0.023437-0.019531-0.03125-0.03125-0.011719-0.011719-0.015625-0.019531-0.027344-0.03125-4.5859-4.5859-4.5859-12.051 0-16.637 4.5859-4.5859 12.051-4.5859 16.637 0 0.019531 0.019532 0.039062 0.035157 0.058594 0.050782 0.019531 0.019531 0.039062 0.035156 0.058594 0.054687l3.9727 3.332c0.011718 0.011719 0.027344 0.019532 0.039062 0.03125 0.019532 0.015626 0.042969 0.03125 0.0625 0.046876 0.03125 0.023437 0.0625 0.042968 0.09375 0.0625 0.019532 0.011718 0.039063 0.027343 0.058594 0.035156 0.050781 0.03125 0.10547 0.058594 0.15625 0.082031 0.003906 0.003906 0.003906 0.003906 0.007812 0.003906 0.054688 0.023438 0.10938 0.046875 0.16016 0.0625 0.015625 0.007813 0.03125 0.011719 0.042969 0.015625 0.042968 0.015625 0.085937 0.027344 0.12891 0.035156 0.015625 0.003907 0.035156 0.007813 0.050781 0.011719 0.042969 0.007813 0.089844 0.015625 0.13281 0.023438 0.015624 0.003906 0.03125 0.003906 0.046874 0.007812 0.058594 0.007813 0.11719 0.011719 0.17969 0.011719 0.058594 0 0.11719-0.003906 0.17969-0.011719 0.015624-0.003906 0.03125-0.003906 0.046874-0.007812 0.046876-0.007813 0.089844-0.011719 0.13281-0.023438 0.015625-0.003906 0.035156-0.007812 0.050781-0.011719 0.042969-0.011718 0.089844-0.023437 0.13281-0.035156 0.015625-0.003906 0.027343-0.007812 0.042969-0.015625 0.054687-0.019531 0.11328-0.039062 0.16797-0.066406 0.054688-0.023437 0.10547-0.050781 0.15625-0.085937 0.019532-0.011719 0.035156-0.023438 0.054688-0.035157 0.03125-0.019531 0.0625-0.042969 0.097656-0.0625 0.019531-0.015625 0.039062-0.03125 0.058594-0.046875 0.015625-0.011718 0.027344-0.019531 0.039062-0.03125l3.9766-3.332c0.023437-0.019531 0.042969-0.039063 0.0625-0.058594 0.019531-0.015625 0.039062-0.03125 0.054687-0.050781 4.5898-4.5742 12.051-4.5742 16.637 0.007812z" />
-            </svg>
-            Favorite
-          </button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="100pt"
+                height="100pt"
+                version="1.1"
+                viewBox="0 0 100 100"
+                class="h-6 w-6 mr-2"
+                fill="white"
+              >
+                <path d="m25.551 29.172c-5.9766 5.9766-5.9961 15.688-0.0625 21.691l0.003907 0.003907 20.738 22.766c0.99219 1.1016 2.3281 1.707 3.7656 1.707s2.7734-0.60547 3.7578-1.6992l20.75-22.773c5.9336-6.0039 5.9141-15.715-0.058594-21.695-5.9688-5.9648-15.668-5.9922-21.672-0.078125l-2.7734 2.3281-2.7734-2.3281c-6.0039-5.9141-15.703-5.8906-21.676 0.078125zm46.34 2.5547c4.5859 4.5859 4.5859 12.051 0 16.637-0.011719 0.011719-0.019531 0.023438-0.03125 0.03125l-0.027344 0.027344-20.758 22.781c-0.29688 0.32812-0.67969 0.51172-1.0742 0.51172-0.39844 0-0.78125-0.18359-1.0859-0.51953l-20.742-22.77c-0.011719-0.011719-0.023437-0.019531-0.03125-0.03125-0.011719-0.011719-0.015625-0.019531-0.027344-0.03125-4.5859-4.5859-4.5859-12.051 0-16.637 4.5859-4.5859 12.051-4.5859 16.637 0 0.019531 0.019532 0.039062 0.035157 0.058594 0.050782 0.019531 0.019531 0.039062 0.035156 0.058594 0.054687l3.9727 3.332c0.011718 0.011719 0.027344 0.019532 0.039062 0.03125 0.019532 0.015626 0.042969 0.03125 0.0625 0.046876 0.03125 0.023437 0.0625 0.042968 0.09375 0.0625 0.019532 0.011718 0.039063 0.027343 0.058594 0.035156 0.050781 0.03125 0.10547 0.058594 0.15625 0.082031 0.003906 0.003906 0.003906 0.003906 0.007812 0.003906 0.054688 0.023438 0.10938 0.046875 0.16016 0.0625 0.015625 0.007813 0.03125 0.011719 0.042969 0.015625 0.042968 0.015625 0.085937 0.027344 0.12891 0.035156 0.015625 0.003907 0.035156 0.007813 0.050781 0.011719 0.042969 0.007813 0.089844 0.015625 0.13281 0.023438 0.015624 0.003906 0.03125 0.003906 0.046874 0.007812 0.058594 0.007813 0.11719 0.011719 0.17969 0.011719 0.058594 0 0.11719-0.003906 0.17969-0.011719 0.015624-0.003906 0.03125-0.003906 0.046874-0.007812 0.046876-0.007813 0.089844-0.011719 0.13281-0.023438 0.015625-0.003906 0.035156-0.007812 0.050781-0.011719 0.042969-0.011718 0.089844-0.023437 0.13281-0.035156 0.015625-0.003906 0.027343-0.007812 0.042969-0.015625 0.054687-0.019531 0.11328-0.039062 0.16797-0.066406 0.054688-0.023437 0.10547-0.050781 0.15625-0.085937 0.019532-0.011719 0.035156-0.023438 0.054688-0.035157 0.03125-0.019531 0.0625-0.042969 0.097656-0.0625 0.019531-0.015625 0.039062-0.03125 0.058594-0.046875 0.015625-0.011718 0.027344-0.019531 0.039062-0.03125l3.9766-3.332c0.023437-0.019531 0.042969-0.039063 0.0625-0.058594 0.019531-0.015625 0.039062-0.03125 0.054687-0.050781 4.5898-4.5742 12.051-4.5742 16.637 0.007812z" />
+              </svg>
+              Favorite
+            </button>
+          </div>
         </div>
       </div>
 
-      <div class="mx-auto mt-8 grid max-w-3xl grid-cols-1 gap-6 sm:px-6 lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-3">
+      <div class="mx-auto mt-4 grid max-w-3xl grid-cols-1 gap-6 lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-3">
         <div class="space-y-6 lg:col-span-2 lg:col-start-1">
           <section aria-labelledby="applicant-information-title">
             <div class="bg-white shadow sm:rounded-lg">
               <div class="px-4 py-5 sm:px-6">
-                <h2
-                  id="applicant-information-title"
-                  class="text-lg font-medium leading-6 text-gray-900"
-                >
-                  Servings per week
-                </h2>
-                <span class="inline-flex items-center rounded-full bg-teal-100 px-3 py-0.5 text-sm font-medium text-teal-800">
-                  {!userDetails.needed &&
-                  userDetails.weight !== undefined &&
-                  userDetails.age !== undefined &&
-                  userDetails.sex !== undefined &&
-                  userDetails.portion !== undefined
-                    ? calculateServings(
-                        userDetails.age,
-                        userDetails.weight,
-                        userDetails.portion,
-                        userDetails.sex,
-                        userDetails.plan_to_get_pregnant || "",
-                        fishData.fish_data
-                      )
-                    : "? servings per week"}
-                </span>
+                <dl class="grid grid-cols-3 gap-x-4 gap-y-8">
+                  <div class="sm:col-span-1">
+                    <div class="flex items-center">
+                      <dt class="text-sm font-medium text-gray-500">Protein</dt>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="100pt"
+                        height="100pt"
+                        version="1.1"
+                        viewBox="0 0 100 100"
+                        class="h-5 cursor-pointer w-5 hover:fill-teal-500"
+                        onClick$={() => alert("heelo")}
+                      >
+                        <path d="m50 16.918c-18.242 0-33.086 14.84-33.086 33.082s14.844 33.082 33.086 33.082 33.086-14.84 33.086-33.082-14.844-33.082-33.086-33.082zm0 62.164c-16.035 0-29.086-13.047-29.086-29.082s13.051-29.082 29.086-29.082 29.086 13.047 29.086 29.082-13.051 29.082-29.086 29.082zm-2-39.832h4v-6.168h-4zm0 27.668h4v-23.418h-4z" />
+                      </svg>
+                    </div>
+                    <dd class="mt-1 text-lg text-gray-900">
+                      {fishData.fish_data.protein.toFixed(3)}
+                      <span class="text-xs text-gray-700">g per 100g</span>
+                    </dd>
+                  </div>
+                  <div class="sm:col-span-1">
+                    <div class="flex items-center">
+                      <dt class="text-sm font-medium text-gray-500">
+                        Omega 3/6 ratio
+                      </dt>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="100pt"
+                        height="100pt"
+                        version="1.1"
+                        viewBox="0 0 100 100"
+                        class="h-5 cursor-pointer w-5 hover:fill-teal-500"
+                        onClick$={() => alert("heelo")}
+                      >
+                        <path d="m50 16.918c-18.242 0-33.086 14.84-33.086 33.082s14.844 33.082 33.086 33.082 33.086-14.84 33.086-33.082-14.844-33.082-33.086-33.082zm0 62.164c-16.035 0-29.086-13.047-29.086-29.082s13.051-29.082 29.086-29.082 29.086 13.047 29.086 29.082-13.051 29.082-29.086 29.082zm-2-39.832h4v-6.168h-4zm0 27.668h4v-23.418h-4z" />
+                      </svg>
+                    </div>
+                    <dd class="mt-1 text-lg text-gray-900">
+                      {fishData.fish_data.omega_3_ratio.toFixed(3)}{" "}
+                    </dd>
+                  </div>
+                  <div class="sm:col-span-1">
+                    <div class="flex items-center">
+                      <dt class="text-sm font-medium text-gray-500">
+                        Servings
+                      </dt>
+                    </div>
+                    <dd class="mt-1 text-lg text-gray-900">
+                      {!userDetails.needed &&
+                      userDetails.weight !== undefined &&
+                      userDetails.age !== undefined &&
+                      userDetails.sex !== undefined &&
+                      userDetails.portion !== undefined
+                        ? calculateServings(
+                            userDetails.age,
+                            userDetails.weight,
+                            userDetails.portion,
+                            userDetails.sex,
+                            userDetails.plan_to_get_pregnant || "",
+                            fishData.fish_data
+                          )
+                        : "? servings per week"}
+                    </dd>
+                  </div>
+                </dl>
               </div>
-              <div class="border-t border-gray-200 px-4 py-5 sm:px-6">
-                <FishDetails fish={fishData.fish_data} index={false} />
+              <div class="border-t border-gray-200 px-4 py-8 sm:px-6">
+                <div class="sm:col-span-2">
+                  <dt class="text-sm font-medium text-gray-500">About</dt>
+                  <dd class="mt-1 text-sm text-gray-900">
+                    Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim
+                    incididunt cillum culpa consequat. Excepteur qui ipsum
+                    aliquip consequat sint. Sit id mollit nulla mollit nostrud
+                    in ea officia proident. Irure nostrud pariatur mollit ad
+                    adipisicing reprehenderit deserunt qui eu.
+                  </dd>
+                </div>
+                <dl class="mt-8 grid grid-cols-2 gap-x-4 gap-y-8">
+                  <div class="sm:col-span-1">
+                    <div class="flex items-center">
+                      <dt class="text-sm font-medium text-gray-500">Mercury</dt>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="100pt"
+                        height="100pt"
+                        version="1.1"
+                        viewBox="0 0 100 100"
+                        class="h-5 cursor-pointer w-5 hover:fill-teal-500"
+                        onClick$={() => alert("heelo")}
+                      >
+                        <path d="m50 16.918c-18.242 0-33.086 14.84-33.086 33.082s14.844 33.082 33.086 33.082 33.086-14.84 33.086-33.082-14.844-33.082-33.086-33.082zm0 62.164c-16.035 0-29.086-13.047-29.086-29.082s13.051-29.082 29.086-29.082 29.086 13.047 29.086 29.082-13.051 29.082-29.086 29.082zm-2-39.832h4v-6.168h-4zm0 27.668h4v-23.418h-4z" />
+                      </svg>
+                    </div>
+                    <dd class="mt-1 text-gray-900">
+                      {fishData.fish_data.mercury.toFixed(3)}
+                      <span class="text-xs text-gray-700"> ppm</span>
+                    </dd>
+                  </div>
+                  <div class="sm:col-span-1">
+                    <div class="flex items-center">
+                      <dt class="text-sm font-medium text-gray-500">PCB</dt>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="100pt"
+                        height="100pt"
+                        version="1.1"
+                        viewBox="0 0 100 100"
+                        class="h-5 cursor-pointer w-5 hover:fill-teal-500"
+                        onClick$={() => alert("heelo")}
+                      >
+                        <path d="m50 16.918c-18.242 0-33.086 14.84-33.086 33.082s14.844 33.082 33.086 33.082 33.086-14.84 33.086-33.082-14.844-33.082-33.086-33.082zm0 62.164c-16.035 0-29.086-13.047-29.086-29.082s13.051-29.082 29.086-29.082 29.086 13.047 29.086 29.082-13.051 29.082-29.086 29.082zm-2-39.832h4v-6.168h-4zm0 27.668h4v-23.418h-4z" />
+                      </svg>
+                    </div>
+                    <dd class="mt-1 text-gray-900">
+                      {fishData.fish_data.pcb.toFixed(3)}{" "}
+                      <span class="text-xs text-gray-700">ppm</span>
+                    </dd>
+                  </div>
+                </dl>
               </div>
             </div>
           </section>
