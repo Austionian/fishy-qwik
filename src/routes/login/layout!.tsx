977da0d3db355919389c607b23f4b9f39f8630a5
Austin Rooks <Austionian@users.noreply.github.com>
2023-04-28 09:07:31 -0500

    add bg

--- src/routes/login/layout!.tsx
+++ src/routes/login/layout!.tsx
@@ -2,11 +2,15 @@ import { component$, Slot } from "@builder.io/qwik";
 
 export default component$(() => {
   return (
-    <div class="fixed w-full h-full bg-gray-800 overflow-auto">
-      <div class="text-center min-h-screen">
-        <main class="w-full md: max-w-xl m-auto">
-          <Slot />
-        </main>
+    <div class="fixed w-full h-full bg-gray-800 overflow-auto bg-[url('/images/bg_low.webp')] bg-cover bg-center">
+      <div class="fixed w-full h-full overflow-auto bg-[url('/images/bg.webp')] bg-cover bg-center">
+        <div class="fixed w-full h-full overflow-auto bg-[url('/images/bg2x.webp')] bg-cover bg-center">
+          <div class="text-center min-h-screen">
+            <main class="w-full md: max-w-xl m-auto">
+              <Slot />
+            </main>
+          </div>
+        </div>
       </div>
     </div>
   );
