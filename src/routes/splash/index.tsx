dd73bcc7d17b88e1c609a5aecd152e9aee35ee1d
Austin Rooks <Austionian@users.noreply.github.com>
2023-04-30 14:14:26 -0500

    refine splash

--- src/routes/splash/index.tsx
+++ src/routes/splash/index.tsx
@@ -4,9 +4,11 @@ export default component$(() => {
   return (
     <div class="container mx-auto sm:px-6 lg:px-8">
       <div class="mt-14 sm:mx-auto sm:w-full sm:max-w-md">
-        <div class="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
-          <div class="pb-10 mt-8">
-            <h1 class="text-5xl text-gray-900 font-bold">Boozhoo!</h1>
+        <div class="bg-white px-4 py-4 sm:py-8 shadow sm:rounded-lg sm:px-10">
+          <div class="pb-2 sm:pb-10 sm:mt-8">
+            <h1 class="text-3xl sm:text-5xl text-gray-900 font-bold">
+              Boozhoo!
+            </h1>
           </div>
           <div class="text-left text-gray-900 px-2">
             <p class="leading-6">
@@ -15,7 +17,7 @@ export default component$(() => {
               Chippewa-Ottawa Resource Authority!
             </p>
           </div>
-          <div class="mt-7 flex justify-center">
+          <div class="mt-2 sm:mt-7 flex justify-center">
             <img
               src="/images/splash.webp"
               alt="Image showing the territories covered by the 1836 Treaty."
@@ -26,7 +28,7 @@ export default component$(() => {
             <a href="/">
               <button
                 class="flex w-full justify-center rounded-md bg-teal-600 px-3 py-2
-                   font-semibold text-gray-900 text-lg shadow-sm hover:bg-teal-500
+                   font-semibold text-white text-lg shadow-sm hover:bg-teal-500 hover:text-gray-100
                    focus-visible:outline focus-visible:outline-2 
                    focus-visible:outline-offset-2 focus-visible:outline-teal-600"
               >
