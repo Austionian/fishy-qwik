d7c5cd79d7b1b5b936d12191d9634464f1c85e4a
Austin Rooks <Austionian@users.noreply.github.com>
2023-05-01 19:23:05 -0500

    blur info modal background

--- src/components/info-modal/info-modal.tsx
+++ src/components/info-modal/info-modal.tsx
@@ -78,7 +78,7 @@ export default component$(
         role="dialog"
         aria-modal="true"
       >
-        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+        <div class="fixed inset-0 bg-black/20 backdrop-blur-sm transition-opacity"></div>
 
         <div class="fixed inset-0 z-10 overflow-y-auto">
           <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
