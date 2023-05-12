3ce9f2e4d0d49cae710f98393227bf563fe7efbf
Austin Rooks <Austionian@users.noreply.github.com>
2023-05-12 16:31:28 -0500

    dynamic settings menu

---
+++ src/components/settings-aside/settings-aside.tsx
@@ -0,0 +1,44 @@
+import { component$ } from "@builder.io/qwik";
+import { useLocation } from "@builder.io/qwik-city";
+import settingsValues from "~/constants/settingsValues";
+
+export default component$(() => {
+  const location = useLocation();
+  const loc = location.url.pathname.split("/");
+  let active = loc[loc.length - 2];
+  if (active === "settings") {
+    active = "profile";
+  }
+  return (
+    <aside class="py-6 lg:col-span-3">
+      <nav class="space-y-1">
+        {settingsValues.map((setting, i) => {
+          if (setting.title.toLowerCase() === active) {
+            return (
+              <a
+                href={setting.link}
+                class="border-teal-500 bg-teal-50 text-teal-700 hover:bg-teal-50 hover:text-teal-700 group flex items-center border-l-4 px-3 py-2 text-sm font-medium"
+                aria-current="page"
+                key={i}
+              >
+                <span dangerouslySetInnerHTML={setting.svgIcon} />
+                <span class="truncate">{setting.title}</span>
+              </a>
+            );
+          }
+          return (
+            <a
+              href={setting.link}
+              class="border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900 group flex items-center border-l-4 px-3 py-2 text-sm font-medium"
+              aria-current="page"
+              key={i}
+            >
+              <span dangerouslySetInnerHTML={setting.svgIcon} />
+              <span class="truncate">{setting.title}</span>
+            </a>
+          );
+        })}
+      </nav>
+    </aside>
+  );
+});
--- src/routes/(app)/settings/index.tsx
+++ src/routes/(app)/settings/index.tsx
@@ -9,6 +9,8 @@ import { v4 as uuidv4 } from "uuid";
 import { getUserDetials } from "~/helpers";
 import type UserDetails from "~/types/UserDetails";
 
+import SettingsAside from "~/components/settings-aside/settings-aside";
+
 export const useUserDetails = routeLoader$<UserDetails>(async ({ cookie }) => {
   return getUserDetials(cookie);
 });
@@ -61,137 +63,7 @@ export default component$(() => {
     <div class="mx-auto max-w-screen-xl px-4 pb-6 sm:px-6 lg:px-8 lg:pb-16">
       <div class="overflow-hidden rounded-lg bg-white shadow">
         <div class="divide-y divide-gray-200 lg:grid lg:grid-cols-12 lg:divide-x lg:divide-y-0">
-          <aside class="py-6 lg:col-span-3">
-            <nav class="space-y-1">
-              <a
-                href="#"
-                class="border-teal-500 bg-teal-50 text-teal-700 hover:bg-teal-50 hover:text-teal-700 group flex items-center border-l-4 px-3 py-2 text-sm font-medium"
-                aria-current="page"
-              >
-                <svg
-                  class="text-teal-500 group-hover:text-teal-500 -ml-1 mr-3 h-6 w-6 flex-shrink-0"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                </svg>
-                <span class="truncate">Profile</span>
-              </a>
-
-              <a
-                href="#"
-                class="border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900 group flex items-center border-l-4 px-3 py-2 text-sm font-medium"
-              >
-                <svg
-                  class="text-gray-400 group-hover:text-gray-500 -ml-1 mr-3 h-6 w-6 flex-shrink-0"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M4.5 12a7.5 7.5 0 0015 0m-15 0a7.5 7.5 0 1115 0m-15 0H3m16.5 0H21m-1.5 0H12m-8.457 3.077l1.41-.513m14.095-5.13l1.41-.513M5.106 17.785l1.15-.964m11.49-9.642l1.149-.964M7.501 19.795l.75-1.3m7.5-12.99l.75-1.3m-6.063 16.658l.26-1.477m2.605-14.772l.26-1.477m0 17.726l-.26-1.477M10.698 4.614l-.26-1.477M16.5 19.794l-.75-1.299M7.5 4.205L12 12m6.894 5.785l-1.149-.964M6.256 7.178l-1.15-.964m15.352 8.864l-1.41-.513M4.954 9.435l-1.41-.514M12.002 12l-3.75 6.495"
-                  />
-                </svg>
-                <span class="truncate">Account</span>
-              </a>
-
-              <a
-                href="/settings/password"
-                class="border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900 group flex items-center border-l-4 px-3 py-2 text-sm font-medium"
-              >
-                <svg
-                  class="text-gray-400 group-hover:text-gray-500 -ml-1 mr-3 h-6 w-6 flex-shrink-0"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z"
-                  />
-                </svg>
-                <span class="truncate">Password</span>
-              </a>
-
-              <a
-                href="#"
-                class="border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900 group flex items-center border-l-4 px-3 py-2 text-sm font-medium"
-              >
-                <svg
-                  class="text-gray-400 group-hover:text-gray-500 -ml-1 mr-3 h-6 w-6 flex-shrink-0"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
-                  />
-                </svg>
-                <span class="truncate">Notifications</span>
-              </a>
-
-              <a
-                href="#"
-                class="border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900 group flex items-center border-l-4 px-3 py-2 text-sm font-medium"
-              >
-                <svg
-                  class="text-gray-400 group-hover:text-gray-500 -ml-1 mr-3 h-6 w-6 flex-shrink-0"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z"
-                  />
-                </svg>
-                <span class="truncate">Billing</span>
-              </a>
-
-              <a
-                href="#"
-                class="border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900 group flex items-center border-l-4 px-3 py-2 text-sm font-medium"
-              >
-                <svg
-                  class="text-gray-400 group-hover:text-gray-500 -ml-1 mr-3 h-6 w-6 flex-shrink-0"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 002.25-2.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v2.25A2.25 2.25 0 006 10.5zm0 9.75h2.25A2.25 2.25 0 0010.5 18v-2.25a2.25 2.25 0 00-2.25-2.25H6a2.25 2.25 0 00-2.25 2.25V18A2.25 2.25 0 006 20.25zm9.75-9.75H18a2.25 2.25 0 002.25-2.25V6A2.25 2.25 0 0018 3.75h-2.25A2.25 2.25 0 0013.5 6v2.25a2.25 2.25 0 002.25 2.25z"
-                  />
-                </svg>
-                <span class="truncate">Integrations</span>
-              </a>
-            </nav>
-          </aside>
-
+          <SettingsAside />
           <form
             class="divide-y divide-gray-200 lg:col-span-9"
             action="#"
