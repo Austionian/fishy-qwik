aa921bd84b257a7b35c34b2b028012be71066d27
Austin Rooks <Austionian@users.noreply.github.com>
2023-05-14 22:54:14 -0500

    prioritize low quality bg image

--- src/routes/(login)/layout.tsx
+++ src/routes/(login)/layout.tsx
@@ -2,7 +2,8 @@ import { component$, Slot } from "@builder.io/qwik";
 
 export default component$(() => {
   return (
-    <div class="fixed w-full h-full bg-gray-800 overflow-auto bg-[url('/images/bg_low.webp')] bg-cover bg-center">
+    <div class="fixed w-full h-full bg-gray-800 overflow-auto bg-cover bg-center">
+      <img src="/images/bg_low.webp" class="hidden" />
       <div class="fixed w-full h-full overflow-auto bg-[url('/images/bg.webp')] bg-cover bg-center">
         <div class="fixed w-full h-full overflow-auto bg-[url('/images/bg2x.webp')] bg-cover bg-center">
           <div class="text-center min-h-screen">
