1678d60b48bbc34f2879b699fc8f74a7e796467e
Austin Rooks <Austionian@users.noreply.github.com>
2023-05-22 10:27:36 -0500

    fix it

--- src/routes/(app)/layout.tsx
+++ src/routes/(app)/layout.tsx
@@ -18,11 +18,8 @@ export const onRequest: RequestHandler = async ({
     }
   } else {
     const user_id = cookie.get("user_id")?.value || "";
-    if (user_id == "") {
-      throw redirect(302, `/login/?redirect=${request.url}`);
-    }
     if (user_id !== GUEST) {
-      const token: string | null = await platform.env.FISHY_KV.get(user_id);
+      const token: string = (await platform.env.FISHY_KV.get(user_id)) || "";
       if (!token || cookie.get("token")?.value !== token) {
         throw redirect(302, `/login/?redirect=${request.url}`);
       }
