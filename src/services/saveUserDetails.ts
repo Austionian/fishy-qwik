e9e66b60bf04a791827a26bd7bb0ce4dc5831de5
Austin Rooks <Austionian@users.noreply.github.com>
2023-05-25 21:04:40 -0500

    save user details to the db

--- src/services/saveUserDetails.ts
+++ src/services/saveUserDetails.ts
@@ -1,4 +1,42 @@
-import { type Cookie } from "@builder.io/qwik-city";
+import { server$, type Cookie } from "@builder.io/qwik-city";
+import { getFetchDetails } from "~/helpers";
+
+const serverSaveUserDetails = server$(async function (
+  user_id: string,
+  weight: number,
+  age: number,
+  sex: string,
+  plan_to_get_pregnant: string,
+  portion: string
+) {
+  const { domain, apiKey } = getFetchDetails(this?.env);
+  const weightStr = weight.toString();
+  const ageStr = age.toString();
+  const plan_to_get_pregnantStr =
+    plan_to_get_pregnant && plan_to_get_pregnant === "Yes" ? "true" : "false";
+  const response = await fetch(`${domain}/v1/save`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${apiKey}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      user_id,
+      weight: weightStr,
+      age: ageStr,
+      sex,
+      plan_to_get_pregnant: plan_to_get_pregnantStr,
+      portion_size: portion,
+    }),
+  });
+  if (!response.ok) {
+    console.error(response.statusText);
+    return {
+      failed: true,
+      formErrors: `Error: ${response.statusText}`,
+    };
+  }
+});
 
 /**
  * Saves the users details to the database and updates the cookies locally.
@@ -19,10 +57,17 @@ const saveUserDetails = (
   const GUEST = "GUEST";
 
   const user_id = cookie.get("user_id")?.value || "";
-  // if (user_id !== "" && user_id !== GUEST) {
-  //   // save to db
-  // }
-  //
+  if (user_id !== "" && user_id !== GUEST) {
+    serverSaveUserDetails(
+      user_id,
+      weight,
+      age,
+      sex,
+      plan_to_get_pregnant,
+      portion
+    );
+  }
+
   const expire_date =
     user_id === GUEST ? ONE_DAY_FROM_TODAY_DATE : TWO_WEEKS_FROM_TODAY_DATE;
   cookie.set("age", age, {
