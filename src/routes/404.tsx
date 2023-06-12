8abeb335ca98577483377743e95801f4fe247a25
Austin Rooks <Austionian@users.noreply.github.com>
2023-06-12 09:58:44 -0500

    turn it on

--- src/routes/404.tsx
+++ src/routes/404.tsx
@@ -9,7 +9,7 @@ export default component$(() => {
     <div class="min-h-screen bg-gradient-to-b from-teal-50 to-white dark:bg-gray-900 dark:from-gray-900/80 dark:to-gray-900/80">
       <main class="pt-10 min-h-full">
         <div class="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
-          <NavBack href={"/"} text={"home"} />
+          <NavBack href={"/"} text={"home"} alwaysShow />
           <Error message={"Page not found."} />
         </div>
       </main>
