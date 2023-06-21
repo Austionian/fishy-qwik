aaa67337fdf81e19cec86ba60f8a73042ff826d9
Austin Rooks <Austionian@users.noreply.github.com>
2023-06-20 22:03:31 -0500

    sort component

---
+++ src/components/sort-svg/sort-svg.tsx
@@ -0,0 +1,30 @@
+import { component$ } from "@builder.io/qwik";
+export default component$(() => (
+  <>
+    <svg
+      class="-ml-0.5 h-5 w-5 text-gray-400 dark:text-gray-200"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+      aria-hidden="true"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M2 3.75A.75.75 0 012.75 3h11.5a.75.75 0 010 1.5H2.75A.75.75 0 012 3.75zM2 7.5a.75.75 0 01.75-.75h6.365a.75.75 0 010 1.5H2.75A.75.75 0 012 7.5zM14 7a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02l-1.95-2.1v6.59a.75.75 0 01-1.5 0V9.66l-1.95 2.1a.75.75 0 11-1.1-1.02l3.25-3.5A.75.75 0 0114 7zM2 11.25a.75.75 0 01.75-.75H7A.75.75 0 017 12H2.75a.75.75 0 01-.75-.75z"
+        clip-rule="evenodd"
+      />
+    </svg>
+    <span class="hidden sm:block dark:text-gray-200">Sort</span>
+    <svg
+      class="-mr-1 h-5 w-5 text-gray-400"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+      aria-hidden="true"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+        clip-rule="evenodd"
+      />
+    </svg>
+  </>
+));
