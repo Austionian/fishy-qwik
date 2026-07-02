8801ee9f2346326e990d556eabe90afbb492a743
Austin Rooks <austin@r00ks.io>
2026-07-02 12:35:56 -0500

    add footer grant info

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,8 +2,11 @@ import { component$ } from "@builder.io/qwik";
 
 export default component$(() => (
   <footer class="text-center">
-    <a href="https://r00ks.io" target="_blank" class="text-xs">
-      Made with ♡ by Shorewood Tech
-    </a>
+    <p>Funding in part by grant #1R01ES028255-01A1</p>
+    <p>
+      <a href="https://r00ks.io" target="_blank" class="text-xs">
+        Made with ♡ by Shorewood Tech
+      </a>
+    </p>
   </footer>
 ));
